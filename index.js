0682971c9558688063c4d695ff4fe535ed66379d
Shashi Lata <100056409+shashilata9519@users.noreply.github.com>
2022-07-27 22:58:13 +0530

    update index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const myfunE = () => {
+  document.getElementById("lang").innerHTML = "ENGLISH SUBTITLE";
+};
+const myfunH = () => {
+  document.getElementById("lang").innerHTML = "हिंदी SUBTITLE";
+};
+const myfunO = () => {
+  document.getElementById("lang").innerHTML = "తెలుగు SUBTITLE";
+};
+const myfunT = () => {
+  document.getElementById("lang").innerHTML = "অসমীয়া SUBTITLE";
+};
+const start = () => {
+  document.getElementById("lang").innerHTML = `<h3> 🎥 start recording 🔴</h3>`;
+};
+const download = () => {
+  document.getElementById(
+    "page"
+  ).innerHTML =`<h4>✔</h4>
+  `
+  alert('downloaded')
+};
+
